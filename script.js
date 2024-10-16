cc7e2fb22c54597a670ff36091b57bf60ebd6558
Yasin <yasinabdi.amir@gmail.com>
2024-10-16 19:45:12 -0400

    add items to cart

---
+++ script.js
@@ -0,0 +1,48 @@
+function addToCart(item1){
+    var item = document.getElementById(item1);
+     
+    if (item1) {
+        item1.style.display = "flex";
+    }
+}
+   
+function addToCart(item2){
+    var item = document.getElementById(item2);
+
+    if (item2) {
+        item2.style.display = "flex";
+    }
+}
+
+function addToCart(item3){
+    var item = document.getElementById(item3);
+
+    if (item3) {
+        item3.style.display = "flex";
+    }
+}
+
+
+function addToCart(item4){
+    var item = document.getElementById(item4);
+
+    if (item4) {
+        item4.style.display = "flex";
+    }
+}
+
+function addToCart(item5){
+    var item = document.getElementById(item5);
+
+    if (item5) {
+        item5.style.display = "flex";
+    }
+}
+
+function addToCart(item6){
+    var item = document.getElementById(item6);
+
+    if (item6) {
+        item6.style.display = "flex";
+    }
+}
