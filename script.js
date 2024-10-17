852e44c93d854fc77b7ab4c2937ac0510fa74312
Yasin <yasinabdi.amir@gmail.com>
2024-10-16 20:13:44 -0400

    remove items

--- script.js
+++ script.js
@@ -46,3 +46,47 @@ function addToCart(item6){
         item6.style.display = "flex";
     }
 }
+
+
+function removeFromCart(item1){
+
+    if (item1) {
+        item1.style.display = "none";
+    }
+}
+
+function removeFromCart(item2){
+
+    if (item2) {
+        item.style.display = "none";
+    }
+}
+
+function removeFromCart(item3){
+
+    if (item3) {
+        item3.style.display = "none";
+    }
+}
+
+function removeFromCart(item4){
+
+    if (item4) {
+        item4.style.display = "none";
+    }
+}
+
+function removeFromCart(item5){
+
+    if (item5) {
+        item5.style.display = "none";
+    }
+}
+
+function removeFromCart(item6){
+
+    if (item6) {
+        item6.style.display = "none";
+    }
+}
+
